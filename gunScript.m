40196c366c88148653172fddc353fdf31bed9cca
Kristian Engberg <krissoengberg@gmail.com>
2017-02-02 14:39:28 +0100

    lyd

--- gunScript.m
+++ gunScript.m
@@ -1,14 +1,21 @@
+classdef gunScript < audioPlugin
+
+    
+    
+end
 filename = 'gunshot.wav'
 [y,Fs] = audioread('gunshot.wav')
 
 
-while(true)
+while(midiread(h))
     
     midiread(h)
+    
     switch h
     case 0
     sound(y,Fs)
-    end
+
+        
     
 
         
